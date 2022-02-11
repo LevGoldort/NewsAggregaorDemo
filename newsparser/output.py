929c6dbad4c65bd274bb16e9389eb3beaac3c7c5
Lev Goldort <lev.goldort@gmail.com>
2022-02-11 19:30:51 +0200

    Added Outuput.py module
    function send_mail connects to SMTP via email and password loaded from config.cfg

---
+++ newsparser/output.py
@@ -0,0 +1,27 @@
+import smtplib
+import ssl
+import json
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def send_mail(email, body):
+
+    with open('config.cfg') as f:
+        config_dict = json.load(f)
+
+    from_email = config_dict['email']
+    password = config_dict['password']
+
+    port = 465
+    context = ssl.create_default_context()
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(body, 'plain'))
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(from_email, password)
+        msg['Subject'] = 'Your news update'
+        msg['From'] = from_email
+        msg['To'] = email
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+
